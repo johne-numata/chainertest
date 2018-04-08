f689b7716919dae20f23a1b8d93a59e90bdf9352
johne-numata <johne.numata@gmail.com>
2018-04-08 18:45:08 +0900

    フォルダー修正

--- LSTM実験/fx_sintest_1.rb
+++ LSTM実験/fx_sintest_1.rb
@@ -3,7 +3,8 @@ include PyCall::Import
 pyimport 'pandas'
 require 'csv'
 
-PyCall.sys.path.append('C:\Users\D6113110\Desktop\chainer\LSTM実験')
+PyCall.sys.path.append(__dir__)
+#PyCall.sys.path.append('C:\Users\D6113110\Desktop\chainer\LSTM実験')
 pyimport 'lstm_class_1', as: :lstm
 
 lstm.train()
